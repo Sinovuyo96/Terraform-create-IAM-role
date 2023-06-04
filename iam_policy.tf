131d895df6f60f22c40008a6fced8e358fcf5923
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2023-06-04 16:39:41 +0200

    Update iam_policy.tf

--- iam_policy.tf
+++ iam_policy.tf
@@ -19,25 +19,6 @@ data "aws_iam_policy_document" "iam_policy" {
     ]
   }
 
-  statement {
-    actions = [
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${var.s3_bucket_name}",
-    ]
-
-    condition {
-      test     = "StringLike"
-      variable = "s3:prefix"
-
-      values = [
-        "",
-        "home/",
-        "home/&{aws:username}/",
-      ]
-    }
   }
 
   statement {
