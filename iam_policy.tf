fc62d1253e7c11d0e043319404192608e77b836e
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2023-06-04 16:35:07 +0200

    Update iam_policy.tf

--- iam_policy.tf
+++ iam_policy.tf
@@ -1,20 +1,53 @@
 resource "aws_iam_policy" "policy" {
+  policy      = data.aws_iam_policy_document.iam_policy.json
   name        = "test_policy"
   path        = "/"
   description = "My test policy"
+}
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
+data "aws_iam_policy_document" "iam_policy" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
     ]
-  })
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}",
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+
+      values = [
+        "",
+        "home/",
+        "home/&{aws:username}/",
+      ]
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}",
+      "arn:aws:s3:::${var.s3_bucket_name}/home/&{aws:username}/*",
+    ]
+  }
 }
